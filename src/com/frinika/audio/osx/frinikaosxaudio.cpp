2d528b73de98dedb41f55f002ada57f440f2fec9
Peter Salomonsen <pjsalomonsen@gmail.com>
2016-05-17 20:29:41 +0200

    Makefile for OSX audio server native lib

--- src/com/frinika/audio/osx/frinikaosxaudio.cpp
+++ src/com/frinika/audio/osx/frinikaosxaudio.cpp
@@ -1,12 +1,27 @@
-//
-//  frinikaosxaudio.cpp
-//  frinikaosxaudio
-//
-//  Created by Peter Salomonsen on 08.05.2016.
-//  Copyright © 2016 Frinika. All rights reserved.
-//
+/*
+ * Created on May 13, 2016
+ *
+ * Copyright (c) 2005-2016 Peter Johan Salomonsen (http://www.petersalomonsen.com)
+ * 
+ * http://www.frinika.com
+ * 
+ * This file is part of Frinika.
+ * 
+ * Frinika is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
 
-#include "frinikaosxaudio.hpp"
+ * Frinika is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Frinika; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include <iostream>
 #include <CoreFoundation/CoreFoundation.h>
 #import <AudioToolbox/AudioToolbox.h>
@@ -54,7 +69,7 @@ OSStatus SineWaveRenderCallback(void * inRefCon,
 
 extern "C" void startAudioWithCallback(FrinikaAudioCallback func) {
     MyAUGraphPlayer *player = {0};
-    MyAUGraphPlayer p = {0};
+    MyAUGraphPlayer p;
     player=&p;
     
     NewAUGraph(&player->graph);
