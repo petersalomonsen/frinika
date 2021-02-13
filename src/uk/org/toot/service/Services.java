dd0b349b4871e141e03ec3d1437067a14811f260
Peter Salomonsen <pjsalomonsen@gmail.com>
2021-02-13 07:48:59 +0100

    fix for running on jdk-14/15

--- src/uk/org/toot/service/Services.java
+++ src/uk/org/toot/service/Services.java
@@ -6,25 +6,15 @@
 package uk.org.toot.service;
 
 import java.util.Iterator;
-import javax.imageio.spi.ServiceRegistry;
+import java.util.ServiceLoader;
 
 /**
  * Services encapsulates the platform=specific provision of service lookup.
  */
-public class Services
-{
-    public static <T extends ServiceProvider> Iterator<T> lookup(Class<T> clazz) {
-		ClassLoader cl = ClassLoader.getSystemClassLoader();
-//      return java.util.ServiceLoader.load(clazz, cl).iterator(); // jse 6
-///		return (Iterator<T>)sun.misc.Service.providers(clazz, cl); // jse 5 sun
-		return ServiceRegistry.lookupProviders(clazz, cl); // jse version compatibility hack !!!
-    }
-
-/*    public static void accept(ServiceVisitor v, Class<?> clazz) {
-        Iterator<T> pit = providers();
-        while ( pit.hasNext() ) {
-            T sp = pit.next();
-            sp.accept(v, clazz);
-        }
-	} */
+public class Services {
+  public static <T extends ServiceProvider> Iterator<T> lookup(Class<T> clazz) {
+    ClassLoader cl = ClassLoader.getSystemClassLoader();
+    ServiceLoader<T> serviceLoader = ServiceLoader.load(clazz);
+    return serviceLoader.iterator();
+  }
 }
